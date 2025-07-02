53fa4a99befe3ef96e48e2acaacc42e77be85e8a
Vladimir Bogdanov <vld.bgdn@icloud.com>
2025-07-02 21:25:54 +0300

    fix production static file issue

--- config/settings.py
+++ config/settings.py
@@ -324,8 +324,13 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
+
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
