2e6974de85ef4e7540aa281ab95df6f81e7b30de
Vladimir Bogdanov <vld.bgdn@icloud.com>
2025-07-03 14:42:56 +0300

    Update cart context processors

--- apps/cart/context_processors.py
+++ apps/cart/context_processors.py
@@ -1,10 +1,7 @@
 from .models import Cart
 
 
-def cart(request):
-    """Add cart to template context"""
-    if not request.session.session_key:
-        request.session.create()
-    session_key = request.session.session_key
-    cart, created = Cart.objects.get_or_create(session_key=session_key)
+def cart_context(request):
+    """Add cart to template context for all views"""
+    cart = Cart.objects.get_or_create_cart(request)
     return {"cart": cart}
